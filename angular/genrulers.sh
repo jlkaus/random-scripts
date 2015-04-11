9d06d74b5a44dab41ebb084ef5fd6e663bcad191
Jonathan L. Kaus <jlkaus@gmail.com>
2015-04-11 14:32:06 -0500

    Add genrulers script and generated svg files

---
+++ angular/genrulers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./angular_vernier_svggen.pl 8.25 10 0 ansvgruler_v 90-0.5 45-0.4 15n0.3 5n0.2 1-0.1 0.5-0.05
+./angular_svggen.pl 8.25 10 0 ansvgruler-0 90-0.5 45-0.4 15n0.3 5n0.2 1-0.1 0.5-0.05
+./angular_svggen.pl 8.25 10 120 ansvgruler-120 90-0.5 45-0.4 15n0.3 5n0.2 1-0.1 0.5-0.05
+./angular_svggen.pl 8.25 10 240 ansvgruler-240 90-0.5 45-0.4 15n0.3 5n0.2 1-0.1 0.5-0.05
